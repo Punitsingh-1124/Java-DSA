3a2e13fa74ec1ca63bffb1da300d2bb13c07752b
Punitsingh-1124 <punitsingh2413@gmail.com>
2025-07-14 12:04:01 +0530

    Time Complexity and Space Complexity

---
+++ Basics of Time Complexity and Space Complexity Java/Time.java
@@ -0,0 +1,98 @@
+// Time Complexity: relation between input size & running Time (operation).
+
+// Q1 Time Complexity 
+// import java.util.Scanner;
+
+// public class Time {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for (int i = 0; i < n; i++) {
+//             System.out.println("hello");
+//         }
+//     }
+// }
+
+// Q2 Time complexity
+//Best Case  - Ω(1)
+//Average Case - Θ(n+1/2)
+//Worst case - O(n)
+
+//Q3 Time Complexity
+// import java.util.Scanner;
+
+// public class Time {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 System.out.println("hello");
+//             }
+//         }
+//     }
+// }
+
+// NxN => O(n)2
+
+//Q4 Time Complexity
+// import java.util.Scanner;
+
+// public class Time {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int m = sc.nextInt();
+
+//         for (int i = 0; i < n; i++) { // n times
+//             for (int j = 0; j < m; j++) { // m Times
+//                 System.out.println("hello");
+//             }
+//         }
+//     }
+// }
+
+// nXm => O(n x m)
+
+//Q5 Time Complexity
+// import java.util.Scanner;
+
+// public class Time {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int m = sc.nextInt();
+//         // Seprate loops
+//         for (int i = 0; i < n; i++) { // n times
+//             System.out.println("Hello");
+//         }
+//         for (int j = 0; j < m; j++) { // m Times
+//             System.out.println("hello");
+//         }
+//     }
+// }
+// O(n + m)
+
+// Q7 Time Complexity
+
+// Compare : O(n) O(n'2) O(n'3)
+//             |    |      |
+// less Time   1    2      3
+
+// Q1 Space Complexity
+// import java.util.Scanner;
+
+// public class Time {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int m = sc.nextInt();
+//         for (int i = 0; i < n; i++) { // n times
+//             System.out.println("Hello");
+//         }
+//     }
+// }
+
+// Q2 Space Complexity
+// Arrays  
