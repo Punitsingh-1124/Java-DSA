f1e19409ae2710f226d1369461cc1389d0a8d18b
Punitsingh-1124 <punitsingh2413@gmail.com>
2025-08-04 12:13:50 +0530

    Selection Sort

--- Sorting in Java/SelectionSort.java
+++ Sorting in Java/SelectionSort.java
@@ -1,4 +1,4 @@
-public class BubbleSort {
+public class SelectionSort {
     public static void printArrays(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -8,20 +8,20 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int arr[] = { 7, 8, 3, 2, 1 };
-
-        // Bubble sort
+        // Selection sort
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // swap
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+            int smallest = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[smallest] > arr[j]) {
+                    smallest = j;
                 }
             }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
         }
         printArrays(arr);
     }
 }
 // 1 2 3 7 8
-//// TC = O(n^2)
+// TC = O(n^2)
