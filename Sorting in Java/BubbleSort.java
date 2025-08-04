b741b8289e2d6efc519dd4f5aa57eeddad52b44e
Punitsingh-1124 <punitsingh2413@gmail.com>
2025-08-04 11:59:13 +0530

    Sorting in Java

---
+++ Sorting in Java/BubbleSort.java
@@ -0,0 +1,25 @@
+public class BubbleSort {
+    public static void printArrays(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 7, 8, 3, 2, 1 };
+
+        // Bubble sort
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printArrays(arr);
+    }
+}
