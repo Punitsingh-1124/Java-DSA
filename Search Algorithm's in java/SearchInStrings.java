01d3fb8b497a670dd8f4f054911628549f5d0e24
Punitsingh-1124 <punitsingh2413@gmail.com>
2025-08-06 13:37:49 +0530

    Search In Strings

--- Search Algorithm's in java/SearchInStrings.java
+++ Search Algorithm's in java/SearchInStrings.java
@@ -1,14 +1,39 @@
+// import java.util.*;
+
+// public class SearchInStrings {
+
+//     public static boolean searchString(String str, char target) {
+//         if (str.length() == 0) {
+//             return false;
+//         }
+
+//         for (int i = 0; i < str.length(); i++) {
+//             if (target == str.charAt(i)) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+
+//     public static void main(String[] args) {
+//         String name = "kunal";
+//         char target = 'u';
+//         System.out.println(searchString(name, target));
+//     }
+// }
+// true
+
 import java.util.*;
 
 public class SearchInStrings {
 
-    public static boolean searchString(String str, char target) {
+    public static boolean searchString2(String str, char target) {
         if (str.length() == 0) {
             return false;
         }
 
-        for (int i = 0; i < str.length(); i++) {
-            if (target == str.charAt(i)) {
+        for (char ch : str.toCharArray()) {
+            if (ch == target) {
                 return true;
             }
         }
@@ -18,7 +43,7 @@ public class SearchInStrings {
     public static void main(String[] args) {
         String name = "kunal";
         char target = 'u';
-        System.out.println(searchString(name, target));
+        System.out.println(Arrays.toString(name.toCharArray()));
     }
 }
-// true
+// [k, u, n, a, l]
