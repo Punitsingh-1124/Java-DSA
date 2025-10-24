397658a6a6ddec6a8c67867a46e3f6a1d0a930d8
Punitsingh-1124 <punitsingh2413@gmail.com>
2025-10-24 21:57:01 +0530

    Binary search in 2D Arrays

--- Search Algorithm's in java/Binary Search Algorithm/BinarySearchin2DArrays.java
+++ Search Algorithm's in java/Binary Search Algorithm/BinarySearchin2DArrays.java
@@ -53,34 +53,119 @@
 
 //Or this one 
 
-import java.lang.reflect.Array;
+// import java.lang.reflect.Array;
+// import java.util.Arrays;
+
+// public class BinarySearchin2DArrays {
+
+//     public static void main(String[] args) {
+//         int[][] matrix = {
+//                 { 10, 20, 30, 40 },
+//                 { 11, 25, 35, 45 },
+//                 { 28, 29, 37, 49 },
+//                 { 33, 34, 38, 50 }
+//         };
+//         System.out.println(Arrays.toString(search(matrix, 50)));
+//     }
+
+//     public static int[] search(int[][] matrix, int target) {
+//         int r = 0;
+//         int c = matrix.length - 1;
+//         while (r < matrix.length && c >= 0) {
+//             if (matrix[r][c] == target) {
+//                 return new int[] { r, c };
+//             }
+//             if (matrix[r][c] < target) {
+//                 r++;
+//             } else {
+//                 c--;
+//             }
+//         }
+//         return new int[] { -1, -1 };
+//     }
+// }
+
+//or Search in  Shorted arrays
 import java.util.Arrays;
 
 public class BinarySearchin2DArrays {
 
     public static void main(String[] args) {
         int[][] matrix = {
-                { 10, 20, 30, 40 },
-                { 11, 25, 35, 45 },
-                { 28, 29, 37, 49 },
-                { 33, 34, 38, 50 }
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 }
         };
-        System.out.println(Arrays.toString(search(matrix, 50)));
+        System.out.println(Arrays.toString(search(matrix, 9)));
     }
 
-    public static int[] search(int[][] matrix, int target) {
-        int r = 0;
-        int c = matrix.length - 1;
-        while (r < matrix.length && c >= 0) {
-            if (matrix[r][c] == target) {
-                return new int[] { r, c };
+    // search in the row provided between the cols provided
+    public static int[] binarySearchInSortedArrays(int[][] matrix, int row, int cStart, int cEnd, int target) {
+        while (cStart <= cEnd) {
+            int mid = cStart + (cEnd - cStart) / 2;
+            if (matrix[row][mid] == target) {
+                return new int[] { row, mid };
             }
-            if (matrix[r][c] < target) {
-                r++;
+            if (matrix[row][mid] < target) {
+                cStart = mid + 1;
             } else {
-                c--;
+                cEnd = mid - 1;
             }
         }
         return new int[] { -1, -1 };
+    }
+
+    public static int[] search(int[][] matrix, int target) {
+        int r = matrix.length;
+        int cols = matrix[0].length;
+
+        // Special case if there's only one row
+        if (r == 1) {
+            return binarySearchInSortedArrays(matrix, 0, 0, cols - 1, target);
+        }
+
+        int rStart = 0;
+        int rEnd = r - 1;
+        int cMid = cols / 2;
+
+        // run the loop until 2 rows are remaining
+        while (rStart < (rEnd - 1)) {
+            int mid = rStart + (rEnd - rStart) / 2;
+            if (matrix[mid][cMid] == target) {
+                return new int[] { mid, cMid };
+            }
+            if (matrix[mid][cMid] < target) {
+                rStart = mid;
+            } else {
+                rEnd = mid;
+            }
+        }
+
+        // now we have two rows to check
+        // check whether the target is in the col of 2 rows
+        if (matrix[rStart][cMid] == target) {
+            return new int[] { rStart, cMid };
+        }
+        if (matrix[rStart + 1][cMid] == target) {
+            return new int[] { rStart + 1, cMid };
+        }
+
+        // search in the last half
+        if (target <= matrix[rStart][cMid - 1]) {
+            return binarySearchInSortedArrays(matrix, rStart, 0, cMid - 1, target);
+        }
+        // search in the 2nd half
+        if (target >= matrix[rStart][cMid + 1] && target <= matrix[rStart][cols - 1]) {
+            return binarySearchInSortedArrays(matrix, rStart, cMid + 1, cols - 1, target);
+        }
+        // search in the 3rd half
+        if (target <= matrix[rStart + 1][cMid - 1]) {
+            return binarySearchInSortedArrays(matrix, rStart + 1, 0, cMid - 1, target);
+        }
+        // search in the 4th half
+        else {
+            return binarySearchInSortedArrays(matrix, rStart + 1, cMid + 1, cols - 1, target);
+        }
     }
 }
